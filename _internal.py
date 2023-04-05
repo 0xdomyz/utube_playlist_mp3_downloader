105211a7c4c14d505c0192f7841cb577e8255ebc
0xdomyz <yzdominic@outlook.com>
2023-04-05 23:25:56 +1000

    use pytube to get target list

--- _internal.py
+++ _internal.py
@@ -2,6 +2,7 @@ import re
 from pathlib import Path
 from typing import List
 
+import pytube
 import yt_dlp
 
 HEADERS = {
@@ -17,20 +18,34 @@ HEADERS = {
 }
 
 
+# def fetch_items_from_list(
+#     webpath: str, start: int = None, end: int = None
+# ) -> List[str]:
+#     headers = {
+#         "flat_playlist": True,
+#         "age_limit": 18,
+#     }
+#     if start is not None:
+#         headers["playliststart"] = start
+#     if end is not None:
+#         headers["playlistend"] = end
+#     with yt_dlp.YoutubeDL(headers) as ydl:
+#         playlist = ydl.extract_info(webpath, download=False)
+#         return [video["webpage_url"] for video in playlist["entries"]]
+
+
 def fetch_items_from_list(
-    webpath: str, start: int = None, end: int = None
+    playlist_url: str, start: int = None, end: int = None
 ) -> List[str]:
-    headers = {
-        "flat_playlist": True,
-        "age_limit": 18,
-    }
-    if start is not None:
-        headers["playliststart"] = start
-    if end is not None:
-        headers["playlistend"] = end
-    with yt_dlp.YoutubeDL(headers) as ydl:
-        playlist = ydl.extract_info(webpath, download=False)
-        return [video["webpage_url"] for video in playlist["entries"]]
+    playlist = pytube.Playlist(playlist_url)
+    res = playlist.video_urls
+    if start is not None and end is not None:
+        res = res[start:end]
+    elif start is not None:
+        res = res[start:]
+    elif end is not None:
+        res = res[:end]
+    return res
 
 
 def already_downloaded_ids(folder: Path) -> List[str]:
