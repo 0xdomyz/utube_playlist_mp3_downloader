243cf32cd7e697b08943d52daf783a6fa1ca917a
0xdomyz <yzdominic@outlook.com>
2023-04-07 16:23:36 +1000

    update version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = "dmp3"
 copyright = "2023, 0xdomyz"
 author = "0xdomyz"
-release = "0.0.3"
+release = "0.0.4"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
