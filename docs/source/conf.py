f860a99c8ac02edd2755d39366fd207436661d55
0xdomyz <yzdominic@outlook.com>
2023-04-09 23:45:36 +1000

    new v number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = "dmp3"
 copyright = "2023, 0xdomyz"
 author = "0xdomyz"
-release = "0.0.6"
+release = "0.0.7"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
